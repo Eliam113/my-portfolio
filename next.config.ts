ed8f5fa6ed311f34aa108633127605042bdbb02e
Eliam Mputu <eliammputu113@gmail.com>
2025-10-24 20:10:10 -0600

    9th try

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || (isProduction ? "/my-portfolio" : ""),
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || (isProduction ? "/my-portfolio" : ""),
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/my-portfolio",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "/my-portfolio",
   output: "export",
   images: { unoptimized: true },
 };
